0e9869fa1af1fb848ee1e057c3bdf04a27bbbb25
Daniel Lovasko <daniel.lovasko@cern.ch>
2014-10-20 18:14:29 +0200

    fast-feature-test: master: work in progress on test running

--- fast-feature-test/master/content/run_tests.sh
+++ fast-feature-test/master/content/run_tests.sh
@@ -42,53 +42,68 @@ function run_tests()
   exit 0
 }
 
-# check input parameters
-if [[ "$#" != 1 || "${1}" = "-h" || "${1}" = "--help" ]]
-then
-  usage
-fi
-
-# determine whether the branch is a PR or not
-# assume branch
-pattern=normal
-
-# check for an integer starting with a non-null digit - a PR number
-if [[ "${1}" =~ ^[1-9][0-9]*$ ]]
-then
-  pattern=pull_request
-fi
-
-# checkout the appropriate branch
-if [[ "${pattern}" = "normal" ]]
-then
-  # test for existence
-  if ! git show-ref --verify --quiet refs/heads/"${1}"
+function parse_arguments()
+{
+  # check input parameters
+  if [[ "$#" != 1 || "${1}" = "-h" || "${1}" = "--help" ]]
   then
-    echo "ERROR: no such branch '${1}'"
-    exit 2
+    usage
   fi
+}
 
-  # perform the checkout
-  echo "Checking out the branch ${1}."
-  git checkout "${1}"
+function determine_pattern()
+{
+  # determine whether the branch is a PR or not
+  # assume branch
+  pattern=normal
 
-elif [[ "${pattern}" = "pull_request" ]]
-then
-  # test for existence
-  if ! git show-ref --verify --quiet refs/remotes/origin/pr/"${1}"
+  # check for an integer starting with a non-null digit - a PR number
+  if [[ "${1}" =~ ^[1-9][0-9]*$ ]]
   then
-    echo "ERROR: no such pull request '${1}'"
-    exit 2
+    pattern=pull_request
   fi
+}
 
-  # perform the checkout
-  echo "Checking out the pull request #${1}."
-  git checkout "pr/${1}"
-fi
+function checkout_branch()
+{
+  # checkout the appropriate branch
+  if [[ "${pattern}" = "normal" ]]
+  then
+    # test for existence
+    if ! git show-ref --verify --quiet refs/heads/"${1}"
+    then
+      echo "ERROR: no such branch '${1}'"
+      exit 2
+    fi
 
-# install the feature branch 
-/code/invenio-devscripts/invenio-make-install
+    # perform the checkout
+    echo "Checking out the branch ${1}."
+    git checkout "${1}"
 
-# run the tests
+  elif [[ "${pattern}" = "pull_request" ]]
+  then
+    # test for existence
+    if ! git show-ref --verify --quiet refs/remotes/origin/pr/"${1}"
+    then
+      echo "ERROR: no such pull request '${1}'"
+      exit 2
+    fi
+
+    # perform the checkout
+    echo "Checking out the pull request #${1}."
+    git checkout "pr/${1}"
+  fi
+}
+
+function install_branch()
+{
+  # install the feature branch 
+  /code/invenio-devscripts/invenio-make-install
+}
+
+parse_arguments
+determine_pattern
+checkout_branch
+install_branch
 run_tests true
 
