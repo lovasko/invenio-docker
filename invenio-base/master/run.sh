d6a6c64db20891c2ea50e2ed625d6901510b63ca
Daniel Lovasko <daniel.lovasko@cern.ch>
2014-10-22 14:59:24 +0200

    base: master: run script

---
+++ invenio-base/master/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run the invenio-base: master image
+
+if [[ $# != 2 ]]
+then
+  echo "Wrong number of arguments."
+  echo "Usage: ./run.sh HTTP_PORT HTTPS_PORT"
+  exit 1
+fi
+
+set -x
+sudo docker run -p "${1}":80 -p "${2}":443 invenio-base:master\
+  --apache-in-foreground
+
