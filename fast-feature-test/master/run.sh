61b75caced1c8f1de58a175666b37525c1c6d942
Dimitri Semitsoglou-Tsiapos <dsemitso@cern.ch>
2014-11-20 15:34:01 +0100

    fast-feature-test: master: script dirname as base for shared directory

--- fast-feature-test/master/run.sh
+++ fast-feature-test/master/run.sh
@@ -7,11 +7,18 @@ if [[ "${1}" == "debug" ]]
 then
   sudo docker run -t -i --entrypoint=/bin/bash fast-feature-test:master -s
 else
-  # assure the existence of the output directory 
-  sudo mkdir -p output
+  # common name for output directory
+  output_folder_name="output"
+  
+  # local output directory 
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  output_folder_path_local="$script_dir/$output_folder_name"
+  sudo mkdir -p "$output_folder_path_local"
+  
+  # docker output directory
+  output_folder_path_docker="/$output_folder_name"
 
-  # TODO get the document directory, not the cwd
-  sudo docker run -h invenio -v `pwd`/output:/output \
+  sudo docker run -h invenio -v "$output_folder_path_local":"$output_folder_path_docker" \
     fast-feature-test:master "$@"
 fi
 
