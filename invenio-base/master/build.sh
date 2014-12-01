cd0d7dba8529c553233742fd7ec94eb839970fd4
Daniel Lovasko <daniel.lovasko@cern.ch>
2014-12-01 15:06:15 +0100

    base: master: change in tags

--- invenio-base/master/build.sh
+++ invenio-base/master/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # build script for the invenio-base:master Docker image
 
+# ensure one non-empty argument
+# TODO we might want to check for [a-zA-Z0-9]*
+if [[ $# != 1 || -z ${1} ]]
+then
+  echo "Usage: ./build.sh TAG"
+  exit 1
+fi
+
 set -x
-sudo docker build --no-cache=true -t invenio-base:master .
+sudo docker build --no-cache=true -t invenio-base-master:"$1" .
 
